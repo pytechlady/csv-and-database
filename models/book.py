b4b8221a6fcc59380bcd1b975702adb72fe7484c
newbietechlady <fumex9910@gmail.com>
2021-11-17 14:46:34 -0800

    fetch-users, fetch-books, fetch-user-by-id, fetch-book-by-ssn

--- models/book.py
+++ models/book.py
@@ -1,2 +1,22 @@
+import psycopg2
+
+
+
 class Book:
-    pass
+    def __init__(self):
+        try:
+            self.conn = psycopg2.connect(database='mydatabase', user='myuser', password='1234', host='localhost', port='5432')
+            self.cursor = self.conn.cursor()
+            
+        except:
+            print('Database not connected')
+            
+        
+            
+    def get_all_book(self):
+        self.cursor.execute('SELECT * FROM books')
+        return self.cursor.fetchall()
+    
+    def get_book(self, id):      
+        self.cursor.execute(f"SELECT * FROM books WHERE id={id}")
+        return self.cursor.fetchall()
